d8e2236a11265365605ad15b7c97541477e7eaf7
Cristian Mattarei <mattarei@stanford.edu>
2018-02-12 11:24:33 -0800

    Added systems generation

--- tools/formal_verification/examples/equivalence_checking/mul_2_pe/run.sh
+++ tools/formal_verification/examples/equivalence_checking/mul_2_pe/run.sh
@@ -1,5 +1,9 @@
 WORKINGDIR=tools/formal_verification/examples/equivalence_checking/mul_2_pe/
-echo "Generating system"
+echo " --> Generating Encoding for System 1"
+./bin/coreir -i $WORKINGDIR/system_1.json -p 'cullgraph,flattentypes,flatten,verifyflattenedtypes' -o $WORKINGDIR/system_1.smt2
+echo " --> Generating Encoding for System 2"
+./bin/coreir -i $WORKINGDIR/system_2.json -p 'cullgraph,flattentypes,flatten,verifyflattenedtypes' -o $WORKINGDIR/system_2.smt2
+echo " --> Generating Encoding for Combined Systems"
 python tools/formal_verification/equivalence_checking.py -if1 $WORKINGDIR/system_1.smt2 -if2 $WORKINGDIR/system_2.smt2 -m $WORKINGDIR/imap.txt > $WORKINGDIR/equivalence.smt2
-echo "Running Equivalence Check"
+echo " --> Running Equivalence Check"
 bash tools/formal_verification/cvc4_check.sh $WORKINGDIR/equivalence.smt2
