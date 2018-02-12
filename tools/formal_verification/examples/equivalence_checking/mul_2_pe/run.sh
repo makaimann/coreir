ca02db48bd4bbfebbc3606c56790509e5e40e6c3
Cristian Mattarei <mattarei@stanford.edu>
2018-02-12 13:28:39 -0800

    Added output filtering in bash run script

--- tools/formal_verification/examples/equivalence_checking/mul_2_pe/run.sh
+++ tools/formal_verification/examples/equivalence_checking/mul_2_pe/run.sh
@@ -1,4 +1,4 @@
-WORKINGDIR=tools/formal_verification/examples/equivalence_checking/mul_2_pe/
+WORKINGDIR=tools/formal_verification/examples/equivalence_checking/mul_2_pe
 echo " --> Generating Encoding for System 1"
 ./bin/coreir -i $WORKINGDIR/system_1.json -p 'cullgraph,flattentypes,flatten,verifyflattenedtypes' -o $WORKINGDIR/system_1.smt2
 echo " --> Generating Encoding for System 2"
@@ -6,4 +6,5 @@ echo " --> Generating Encoding for System 2"
 echo " --> Generating Encoding for Combined Systems"
 python tools/formal_verification/equivalence_checking.py -if1 $WORKINGDIR/system_1.smt2 -if2 $WORKINGDIR/system_2.smt2 -m $WORKINGDIR/imap.txt > $WORKINGDIR/equivalence.smt2
 echo " --> Running Equivalence Check"
-bash tools/formal_verification/cvc4_check.sh $WORKINGDIR/equivalence.smt2
+(bash tools/formal_verification/cvc4_check.sh $WORKINGDIR/equivalence.smt2 | python tools/formal_verification/equivalence_compare.py) > $WORKINGDIR/output.txt
+echo " --> Output stored in "$WORKINGDIR/output.txt
